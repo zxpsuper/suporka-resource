ee194ac58539619d8dc17e42f91210ebe4610f0b
zhengxiaopeng <zxpscau@163.com>
2026-05-07 14:07:10 +0800

    fix: 修复跳转错误

--- themes/anzhiyu/scripts/helpers/catalog_list.js
+++ themes/anzhiyu/scripts/helpers/catalog_list.js
@@ -2,8 +2,8 @@ hexo.extend.helper.register("catalog_list", function (type) {
   let html = ``;
   hexo.locals.get(type).map(function (item) {
     html += `
-    <div class="catalog-list-item" id="/${item.path}">
-      <a href="/${item.path}">
+    <div class="catalog-list-item" id="${hexo.config.root}${item.path}">
+      <a href="${hexo.config.root}${item.path}">
         ${item.name}
       </a>
     </div>
